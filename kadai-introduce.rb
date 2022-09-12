4a9ca325bf42d5943a75a0ce5f5c5c6c18e9444c
kohei.mori <hetareinkya@gmail.com>
2022-09-12 13:10:55 +0000

    kadai

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,8 +1,8 @@
-glastname = '森'
+lastname = '森'
 firstname = '康平'
 lastname += firstname #名前
 age = 30
 str1 = 'です。'
 str2 = '歳です。'
 lastname += str1
-puts lastname + age.to_s + str2 #変更しました
+puts lastname + age.to_s + str2
